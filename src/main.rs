13ef941104b0c5b119335e152b7b968ce81d8f96
Aditya <codecommander03@gmail.com>
2024-02-11 18:46:15 +0530

    main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use postgres::{ Client, NoTls };
+use postgres::Error as PostgresError;
+use std::net::{ TcpListener, TcpStream };
+use std::io::{ Read, Write };
+use std::env;
+
+#[macro_use]
+extern crate serde_derive;
+
+// Model: User struct with id, name, email
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: Option<i32>,
+    name: String,
+    email: String,
+}
+
+// DATABASE_URL
+const DB_URL: &str = env!("DATABASE_URL");
+
+//constants
+const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
+const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+const INTERNAL_SERVER_ERROR: &str = "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n";
+
+// main function
 fn main() {
-    println!("Hello, world!");
+    // set database
+    if let Err(e) = set_database() {
+        println!("Error: {}", e);
+        return;
+    }
+
+    // start server and print port
+    let listener = TcpListener::bind(format!("0.0.0.0:8080")).unwrap();
+    println!("Server started at port 8080");
+
+    // handle the client
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
 }
